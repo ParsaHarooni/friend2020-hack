d805a24bf1fd40e9ad73c2f23bd99b8656930de3
Parsa <prsishere@gmail.com>
2020-03-06 01:10:15 +0330

    create web api and some sort of webpage for everyone's use

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
 # TODOS
 
-- [ ] Check if url is for friend2020.com
+- [x] Check if url is for friend2020.com
+- [ ] Create template for website
+- [ ] Make some sort of mass crawler to get some data about people answers :)
--- app/web.py
+++ app/web.py
@@ -1,8 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 from friend2020.results import FriendResult
 
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def main():
+    return render_template("main.html")
+
+@app.route('/result')
+def show_answers():
+    code = request.args.get("code")
+    quiz_result = FriendResult(code).get_answers()
+    resp = dict(status=True, data=quiz_result)
+    if quiz_result is None:
+        resp = dict(status=False, message="Quiz with this code was not found")
+    return jsonify(resp)
 
 @app.route('/api/results/<int:code>')
 def get_results(code):
@@ -12,4 +26,4 @@ def get_results(code):
         resp = dict(status=False, message="Quiz with this code was not found")
     return jsonify(resp)
 
-
+app.run(debug=True)
